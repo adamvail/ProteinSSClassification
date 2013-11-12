79e14d6daa9df8a944c9a07a276895da9cbc4dac
Adam Vail <adam.r.vail@gmail.com>
2013-11-12 13:57:05 -0600

    initial code commit

---
+++ ProteinSSClassification/src/classification/ProteinSSClassification.java
@@ -0,0 +1,14 @@
+package classification;
+
+public class ProteinSSClassification {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Here is the start of our protein secondary
+		// structure classification project
+
+	}
+
+}
