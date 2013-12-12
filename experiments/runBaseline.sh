c8dc1a684b509ad842df5acd9e65385429b271f4
Adam Vail <adam.r.vail@gmail.com>
2013-12-12 16:58:38 -0600

    baseline script now uses all training data instead of just rost data

--- experiments/runBaseline.sh
+++ experiments/runBaseline.sh
@@ -3,10 +3,12 @@
 # Usage: <protein train> <protein test> 
 #                 <window size> <number of hidden layers> <hidden layer size> <iterations> <output directory> <baseline>
 
-rostTrainFilename="/Users/vail/Documents/school/cs760/ProteinSSClassification/data/rost/protein_data"
-uciTrainFilename="/Users/vail/Documents/school/cs760/ProteinSSClassification/data/uci/uci_protein_data.train"
+#rostTrainFilename="/Users/vail/Documents/school/cs760/ProteinSSClassification/data/rost/protein_data"
+#uciTrainFilename="/Users/vail/Documents/school/cs760/ProteinSSClassification/data/uci/uci_protein_data.train"
 uciTestFilename="/Users/vail/Documents/school/cs760/ProteinSSClassification/data/uci/uci_protein_data.test"
-outputDirectory="/Users/vail/Desktop/DeepNetworkData"
+
+trainFilename="/home/vail/Documents/school/cs760/ProteinSSClassification/data/data.train"
+outputDirectory="/home/vail/Documents/school/cs760/ProteinSSClassification/results/baseline"
 
 numHiddenLayers=1
 hiddenLayerSize=50 # jump this by 5 all the way up to 50
@@ -17,14 +19,14 @@ baseline="y"
 
 # Baseline experiments
 
-for i in {5..50..5}
+for i in {15..300..15}
 do
     echo "Running Baseline: hidden layer size -> $i" >> "${outputDirectory}/log"
-    java -jar deepnetwork.jar $rostTrainFilename "none" $windowSize $numHiddenLayers $i $iterations $outputDirectory $baseline
+    java -jar deepnetwork.jar $trainFilename "none" $windowSize $numHiddenLayers $i $iterations $outputDirectory $baseline
 done
 
 for i in 7 11 13 17
 do
     echo "Running Baseline: window size -> $i" >> "${outputDirectory}/log"
-    java -jar deepnetwork.jar $rostTrainFilename "none" $i $numHiddenLayers $hiddenLayerSize $iterations $outputDirectory $baseline
+    java -jar deepnetwork.jar $trainFilename "none" $i $numHiddenLayers $hiddenLayerSize $iterations $outputDirectory $baseline
 done
