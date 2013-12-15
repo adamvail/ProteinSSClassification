b87bfe04f543e509799d61f73f2c882ec1bb8e27
Adam Vail <adam.r.vail@gmail.com>
2013-12-15 10:06:36 -0600

    commiting data from yesterday's runs

---
+++ experiments/runAutoencoderLayerTest_Adam.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Usage: <protein train> <protein test> 
+#                 <window size> <number of hidden layers> <hidden layer size> <iterations> <output directory> <baseline>
+
+trainFilename="/home/vail/Documents/school/cs760/ProteinSSClassification/data/data.train"
+outputDirectory="/home/vail/Documents/school/cs760/ProteinSSClassification/results/deep/layers_adam"
+
+numHiddenLayers=2
+hiddenLayerSize=26 # default value provided by baseline tests
+iterations=5 # increase the number of iterations as we go
+windowSize=13
+baseline="n"
+connectedOutput="n"
+
+
+
+for i in 8 10
+do
+    outputFile="${outputDirectory}/deep_${i}nhl_${hiddenLayerSize}hls_${windowSize}ws"
+    echo "Running Deep Network: hidden layer size 26, layer number -> $i" >> "${outputDirectory}/deep.log"
+    java -jar deepNetwork.jar $trainFilename "none" $windowSize $i $hiddenLayerSize $iterations $outputDirectory $baseline $connectedOutput > $outputFile
+    echo "Done" >> "${outputDirectory}/deep.log"
+done
